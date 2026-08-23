db74e47b49656d4a946b900e87e3651c5d22cd9b
Retoshi Kuzei <200499355+Lammk@users.noreply.github.com>
2026-08-23 10:16:35 +0700

    Delete

--- tests/apk_triangle/test_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -z "$ANDROID_NDK_HOME" ] && [ -n "$ANDROID_NDK_LATEST_HOME" ]; then
-    export ANDROID_NDK_HOME="$ANDROID_NDK_LATEST_HOME"
-fi
-echo $ANDROID_NDK_HOME
--- tests/multi_elf_provider.c
+++
@@ -1,3 +0,0 @@
-int kudroid_dependency_value(void) {
-    return 35;
-}
